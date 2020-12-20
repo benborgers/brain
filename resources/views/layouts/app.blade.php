e15890ad310341d48a896da3d2003345fda20a37
Ben Borgers <borgersbenjamin@gmail.com>
2020-12-20 17:54:12 -0500

    Increase notecard title size + click title for note

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         <style>[x-cloak] { display: none !important; }</style>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,600;0,700;0,800;1,400;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,600;0,700;0,800;0,900;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
         <script src="{{ mix('js/app.js') }}" defer></script>
 
--- resources/views/livewire/notecard.blade.php
+++ resources/views/livewire/notecard.blade.php
@@ -35,25 +35,19 @@
                 x-init="$refs.markdown.innerHTML = $refs.markdown.innerHTML.replace(/\$\$(.*?)\$\$/g, (_, equation) =>
                     katex.renderToString(equation, { throwOnError: false }))"
             >
-                <h1 class="text-2xl font-extrabold text-gray-900 mb-4">{{ $notecard->title }}</h1>
+                @if($embedded) <a href="{{ route('notecard.show', $notecard) }}"> @endif
+                    <h1 class="text-3xl font-extrabold text-gray-900 mb-4">{{ $notecard->title }}</h1>
+                @if($embedded) </a> @endif
                 <div class="prose" x-ref="markdown">
                     {!! $notecard->toHtml() !!}
                 </div>
             </div>
         @elseif($mode === 'edit')
             <form wire:submit.prevent="save" class="space-y-4">
-                <input type="text" wire:model.defer="notecard.title" placeholder="Notecard title" class="w-full p-0 border-none focus:ring-0 text-2xl font-extrabold text-gray-900" />
+                <input type="text" wire:model.defer="notecard.title" placeholder="Notecard title" class="w-full p-0 border-none focus:ring-0 text-3xl font-extrabold text-gray-900" />
                 <textarea wire:model.defer="notecard.markdown" placeholder="# Markdown content" class="w-full p-0 border-none focus:ring-0 resize-none h-64 font-mono text-gray-900"></textarea>
                 <input type="submit" value="{{ $this->notecard->exists ? 'Save' : 'Create' }}" class="cursor-pointer bg-rose-200 text-rose-700 py-1 px-3 rounded-lg font-semibold focus:outline-none">
             </form>
-        @endif
-
-        @if($embedded)
-            <div class="flex justify-end mt-4">
-                <a href="{{ route('notecard.show', $notecard) }}" class="block">
-                    <x-heroicon-o-arrows-expand class="h-4 text-gray-400 hover:text-gray-800 duration-100 transition-colors" />
-                </a>
-            </div>
         @endif
     </div>
 </div>
