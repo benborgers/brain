50b6d25084d04bf908c0fa2e01e4d248b26e2119
Ben Borgers <borgersbenjamin@gmail.com>
2020-12-20 17:34:53 -0500

    Remove error message for sidebar new folder input

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -8,9 +8,6 @@
     {{-- Create new folder --}}
     <form wire:submit.prevent="createFolder" class="mt-12 flex space-x-2 w-full bg-white border border-gray-200 p-3 rounded-lg">
         <input type="text" placeholder="New Folder" wire:model.defer="newFolderName" class="w-full bg-transparent border-none p-0 focus:ring-0" />
-        @error('newFolderName')
-            <p>{{ $message }}</p>
-        @enderror
-        <input type="submit" value="Create" class="cursor-pointer bg-gray-200 text-gray-700 py-1 px-2 rounded text-sm font-semibold" />
+        <input type="submit" value="Create" class="cursor-pointer bg-gray-200 text-gray-700 py-1 px-2 rounded text-sm font-semibold focus:outline-none" />
     </form>
 </div>
