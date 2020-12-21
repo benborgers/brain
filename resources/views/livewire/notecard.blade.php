a1a99b4af3228af23c4a5d6900716b6b72c996f4
Ben Borgers <borgersbenjamin@gmail.com>
2020-12-20 19:45:55 -0500

    Allow adding tabs in notecard editor

--- resources/views/livewire/notecard.blade.php
+++ resources/views/livewire/notecard.blade.php
@@ -46,8 +46,20 @@
             </div>
         @elseif($mode === 'edit')
             <form wire:submit.prevent="save" class="space-y-4">
-                <input type="text" wire:model.defer="notecard.title" placeholder="Notecard title" class="w-full p-0 border-none focus:ring-0 text-3xl font-extrabold text-gray-900" />
-                <textarea wire:model.defer="notecard.markdown" placeholder="# Markdown content" class="w-full p-0 border-none focus:ring-0 resize-none h-64 font-mono text-gray-900"></textarea>
+                <input
+                    type="text"
+                    autofocus
+                    wire:model.defer="notecard.title"
+                    placeholder="Title"
+                    class="w-full p-0 border-none focus:ring-0 text-3xl font-extrabold text-gray-900"
+                />
+                <textarea
+                    wire:model.defer="notecard.markdown"
+                    placeholder="# Markdown content"
+                    class="w-full p-0 border-none focus:ring-0 resize-none h-64 font-mono text-gray-900"
+                    x-data
+                    x-on:keydown.tab.prevent="$el.setRangeText('  ', $el.selectionStart, $el.selectionStart, 'end')"
+                ></textarea>
                 <input type="submit" value="{{ $this->notecard->exists ? 'Save' : 'Create' }}" class="cursor-pointer bg-rose-200 text-rose-700 py-1 px-3 rounded-lg font-semibold focus:outline-none">
             </form>
         @endif
