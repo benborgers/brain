24e978fa665da1f0edc73b00c08e531f934c60a8
Ben Borgers <borgersbenjamin@gmail.com>
2020-12-21 13:52:48 -0500

    Make sidebar new folder button smaller

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -6,8 +6,8 @@
     <x-folder-level :folders="$allFolders->whereNull('parent')" :allFolders="$allFolders" />
 
     {{-- Create new folder --}}
-    <form wire:submit.prevent="createFolder" class="mt-12 flex space-x-2 w-full bg-white border border-gray-200 p-3 rounded-lg">
+    <form wire:submit.prevent="createFolder" class="mt-12 flex space-x-2 w-full bg-white border border-gray-200 py-2 px-3 rounded-lg">
         <input type="text" placeholder="New Folder" wire:model.defer="newFolderName" class="w-full bg-transparent border-none p-0 focus:ring-0" />
-        <input type="submit" value="Create" class="cursor-pointer bg-gray-200 text-gray-700 py-1 px-2 rounded text-sm font-semibold focus:outline-none" />
+        <input type="submit" value="Create" class="cursor-pointer bg-gray-200 text-gray-700 py-1 px-2 rounded text-sm font-semibold focus:outline-none leading-none" />
     </form>
 </div>
