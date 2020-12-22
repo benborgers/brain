825cf95f98fa3b368be2df5f2dbe0846d4120aa2
Ben Borgers <borgersbenjamin@gmail.com>
2020-12-21 20:10:05 -0500

    Add smart punctuation

--- app/Models/Notecard.php
+++ app/Models/Notecard.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment;
+use League\CommonMark\Extension\SmartPunct\SmartPunctExtension;
 
 class Notecard extends Model
 {
@@ -15,9 +17,9 @@ class Notecard extends Model
 
     public function toHtml()
     {
-        $converter = new CommonMarkConverter([
-            'html_input' => 'escape'
-        ]);
+        $environment = Environment::createCommonMarkEnvironment();
+        $environment->addExtension(new SmartPunctExtension());
+        $converter = new CommonMarkConverter([ 'html_input' => 'escape' ], $environment);
 
         $html = $converter->convertToHTML(
             $this->markdown
